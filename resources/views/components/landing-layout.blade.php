1cb608eaf0c7897f543d6af6a0adbb2692d608bb
Rakesh Al Yadin <shopno8860@gmail.com>
2026-01-20 03:53:28 +0600

    feat: user button fixed when logged in in pricing and docs button it does not work.now it work

--- resources/views/components/landing-layout.blade.php
+++ resources/views/components/landing-layout.blade.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&display=swap" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
     <script>
         tailwind.config = {
             theme: {
